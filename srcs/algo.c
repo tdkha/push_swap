728e4a8651de882f0ab0fbbbd63455fa2eab7f14
tieud1 <duykha.tieu@aalto.fi>
2024-07-10 09:45:37 +0300

    next step is to build a function to update the chunk in algo.c

--- srcs/algo.c
+++ srcs/algo.c
@@ -6,7 +6,7 @@
 /*   By: ktieu <ktieu@student.hive.fi>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 11:47:16 by ktieu             #+#    #+#             */
-/*   Updated: 2024/07/09 19:05:06 by ktieu            ###   ########.fr       */
+/*   Updated: 2024/07/10 09:32:08 by ktieu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -102,13 +102,83 @@ void	max_to_top(
 	ps_stack_action("pa",a ,b);
 }
 
+
+void compute_distance(
+	t_chunk *chunk,
+	t_stack *b,
+	int *cmp_dists,
+	int *choose_min
+)
+{
+	int	dist_from_bot[2] = {0};
+	int	dist_from_top[2] = {0};
+
+	dist_from_top[0] = distance_from_top(chunk->min, b);
+	dist_from_top[1] = distance_from_top(chunk->max, b);
+	dist_from_bot[0] = distance_from_bot(chunk->min, b);
+	dist_from_bot[1] = distance_from_bot(chunk->max, b);
+	if (dist_from_top[0] < dist_from_top[1])
+	{
+		cmp_dists[1] = dist_from_top[0];
+		*choose_min = 1;
+	}
+	else
+		cmp_dists[1] = dist_from_top[1];
+	if (dist_from_bot[0] < dist_from_bot[1])
+	{
+		cmp_dists[0] = dist_from_bot[0] + 1;
+	}
+	else
+		cmp_dists[0] = dist_from_bot[1] + 1;
+	if (cmp_dists[0] < cmp_dists[1])
+		*choose_min = 1;
+	printf("Min top: %d\n", dist_from_top[0]);
+	printf("Max top: %d\n", dist_from_top[1]);
+	printf("Min bot: %d\n", dist_from_bot[0]);
+	printf("Max bot: %d\n", dist_from_bot[1]);
+}
+
 void	b2a_push(
 	t_chunk *chunk,
 	t_stack *a,
 	t_stack *b
 )
 {
+	int	cmp_dists[2] = {0};
+	int	bot_is_shorter;
+	int	choose_min;
+	int	iteration;
 
+	bot_is_shorter = 0;
+	choose_min = 0;
+	compute_distance(chunk, b, cmp_dists, &choose_min);
+	if (cmp_dists[0] < cmp_dists[1])
+	{
+		bot_is_shorter = 1;
+		iteration = cmp_dists[0];
+	}
+	else
+		iteration = cmp_dists[1];
+	if (bot_is_shorter)
+	{
+		printf("Choose bot\n");
+		while (iteration-- > 0)
+			ps_stack_action("rrb", a, b);
+	}
+	else
+	{
+		printf("Choose top\n");
+		while (iteration-- > 0)
+			ps_stack_action("rb", a, b);
+	}
+	ps_stack_action("pa",a ,b);
+	if (choose_min)
+	{
+		chunk->min++;
+		ps_stack_action("ra", a, b);
+	}
+	else
+		chunk->max--;
 }
 
 
@@ -119,9 +189,21 @@ void	ps_b2a(t_data *data, t_stack *a, t_stack *b)
 	chunk.max = b->size;
 	chunk.min = chunk.max - data->amt_per_chunk;
 	chunk.median = (chunk.max + chunk.min) / 2;
-	while (b->size)
+	int i = 0;
+	while (i < 10)
 	{
-		max_to_top(&chunk, a, b);
+		printf("----------------------------\n");
+		printf("Chunk max: %d\n", chunk.max);
+		printf("Chunk min: %d\n", chunk.min);
+		printf("----------------------------\n");
+		b2a_push(&chunk, a, b);
+		ft_debug_print_stacks(a, b);
+		if (chunk.max == chunk.min)
+		{
+			// update chunk
+		}
+		i++;
+		// max_to_top(&chunk, a, b);
 	}
 }
 
